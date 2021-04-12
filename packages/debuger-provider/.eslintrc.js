e575acc12aa666f58348942e8aa79774fa67852c
yyjazsf <admin@yingyj.com>
2021-04-12 19:42:54 +0800

    feat: init provider-npm-package code

---
+++ packages/debuger-provider/.eslintrc.js
@@ -0,0 +1,18 @@
+export default {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+  ],
+};
